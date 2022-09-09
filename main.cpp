b284d039d9cdff02d5572185ca793069b819678a
gergohaszonits <71815279+gergohaszonits@users.noreply.github.com>
2022-09-09 19:41:53 +0200

    add comments

--- main.cpp
+++ main.cpp
@@ -1,26 +1,62 @@
-#include <iostream>
-
-constexpr int N_ELEMENTS = 100;
-
 int main()
 {
+    // HIBA: szintaktikai hiba : változó név elírva
+    /*
+            NELEMENTS -> N_ELEMENTS
+    */
     int *b = new int[NELEMENTS];
+    // HIBA: aposztrófok használata idézőjelek helyett
+    /*
+             '1-100 ertekek duplazasa' ->  "1-100 ertekek duplazasa"
+    */
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+   // HIBA: szintaktikai hiba a for ciklusnál
+    /*
+            feltétel magadás és inkrementálás hiányzik
+    */
+    for (int i = 0;)                           
     {
-        b[i] = i * 2;
+      // HIBA: hibás hivatkozás b tömb i-edik elemére
+        // HIBA: 0 érték a tömb 0. eleme
+        /*
+            
+                értékre hivatkozunk nem memóriacimre ezért * al kell jelölni
+                i hez +1 kell, hogy 1-100 -as intervallumban maradjunk
+
+        */
+        b[i] = i * 2;                                             
     }
-    for (int i = 0; i; i++)
+     // HIBA: szintaktikai hiba for ciklus feltétele hiányos
+    /*
+        
+            < N_ELEMENTS-et hozzá kell adni a feltételbe
+           
+    */
+    for (int i = 0; i; i++)                                       
     {
-        std::cout << "Ertek:"
+         // HIBA: lemaradt a kiiratas vegerol a hivatkozas az ertekre
+         /*
+            hozza kell adni:  << *(b + i) << '\n';
+         */
+        std::cout << "Ertek:"                                     
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    std::cout << "Atlag szamitasa: " << std::endl;    
+    // HIBA: atlag valtozo nem lett inicializálva
+    /*
+            atlag = 0;
+            (szükséges mert ahhoz, hogy hozzáadjunk már kell hogy legyen értéke)
+    */          
+    int atlag;      
+     // HIBA: , használata ; helyett                                              
+    for (int i = 0; i < N_ELEMENTS, i++)                          
     {
-        atlag += b[i]
+        // HIBA: hibás hivatkozás b tömb i-edik elemére
+        
+        /*
+                értékre hivatkozunk nem memóriacimre ezért * al kell jelölni
+                és sor végén ;-t hozzaadni
+        */
+        atlag += b[i]                                             
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
-}
